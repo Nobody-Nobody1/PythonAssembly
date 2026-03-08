5577c5fcf087f6f87927e4dc22b7910c89e2d958
Nobody_Nobody1 <bibhasn123@gmail.com>
2026-03-08 06:24:28 -0700

    adding html files to display and edit
    needs work on the execute button and needs work to run VmPython by passing the text area content to it, kinda difficult since it's not specifically with python but i want to connect them.

--- Server.py
+++ Server.py
@@ -1,7 +1,30 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-@app.route('/')
-def home():
-   return "Hello, this is Python running in your browser!"
-if __name__ == '__main__':
-   app.run(debug=True)
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+hostName = "localhost"
+serverPort = 8080
+
+def read_html_file(file_path):
+            with open(file_path, 'r') as file:
+                return file.read()
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        def read_html_file(file_path):
+            with open(file_path, 'r') as file:
+                return file.read()
+        self.wfile.write(bytes(read_html_file("index.html"), "utf-8"))
+
+if __name__ == "__main__":        
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
